afdae519cf37cb76a716818b63be242862757acf
Abdullateef <yomlateef@yahoo.com>
2018-08-04 09:20:48 +0100

    .

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*Navigation opener, its sets width of side-nav to 80% when menu-icon is pressed*/
+function openNav(){
+    document.getElementById('sideNav').style.right = "0";
+    document.getElementById('navLeft').style.left = "0";
+}
+
+/*Navigation closer, it sets width of side-nav to 0 when close-btn is pressed*/
+function closeNav(){
+    document.getElementById('sideNav').style.right = "-80%";
+    document.getElementById('navLeft').style.left = "-20%";    
+}
