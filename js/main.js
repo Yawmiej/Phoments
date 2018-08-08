627019fdf339706565d82a06a58f768d562388d0
Abdullateef <yomlateef@yahoo.com>
2018-08-08 12:47:00 +0100

    Commit before going to ikorodu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//Section1: This section is for Navigation menu, start
+
 /*Navigation opener, its sets width of side-nav to 80% when menu-icon is pressed*/
 function openNav(){
     document.getElementById('sideNav').style.right = "0";
@@ -9,3 +11,5 @@ function closeNav(){
     document.getElementById('sideNav').style.right = "-80%";
     document.getElementById('navLeft').style.left = "-20%";    
 }
+
+//Section1 ends here
